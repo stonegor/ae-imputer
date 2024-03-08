537ec9341164d63d80fb610a30edb341b82f1f70
stonegor <sstonegor@gmail.com>
2024-03-08 19:59:41 +0300

    refactor: moved training and inference params to Imputer constructor to align with sklearn conventions

--- aeimputer/imputer.py
+++ aeimputer/imputer.py
@@ -11,25 +11,22 @@ import pandas as pd
 
 # TODO: Categorical variables support, early stoping
 # ELBO
-# _BaseImputer, fit and transorm
-# move params to constructor
 
 class _BaseImputer:
     def __init__(self, missing_values):
         self.missing_values = missing_values
 
-    def fit(self, X):
-        raise NotImplementedError("This is an abstract method")
+    def fit(self, X, verbose = False):
+        raise NotImplementedError("Abstract method")
 
-    def transform(self, X):
-        raise NotImplementedError("This is an abstract method")
+    def transform(self, X, verbose = False) -> np.ndarray:
+        raise NotImplementedError("Abstract method")
 
-    def fit_transform(self, X, **kwargs):
-        self.fit(X, **kwargs)
-        return self.transform(X,**kwargs)
+    def fit_transform(self, X) -> np.ndarray:
+        self.fit(X)
+        return self.transform(X)
     
     def _format_input(self, X):
-        
         if isinstance(X, list):
             X = np.array(X)
         elif isinstance(X, pd.DataFrame):
@@ -37,10 +34,10 @@ class _BaseImputer:
         if isinstance(X, np.ndarray):
             X = X.copy()
         else:
-            raise TypeError("Excpeceted X to be np.ndarray, list or pd.DataFrame.")
+            raise TypeError("Expected X to be np.ndarray, list or pd.DataFrame.")
             
         if len(X.shape) != 2:
-            raise ValueError(f"Excpeceted X to be of shape (batch, features), got {X.shape} instead.")
+            raise ValueError(f"Expected X to be of shape (batch, features), got {X.shape} instead.")
         
         X = X.astype(np.float32)
         
@@ -48,28 +45,37 @@ class _BaseImputer:
         
         return X 
 
+
 class AEImputer(_BaseImputer):
-    def __init__(self, missing_values = np.nan, n_layers = 3, hidden_dims = None, latent_dim_percentage = 'auto') -> None:
+    def __init__(self, missing_values = np.nan, n_layers = 3, hidden_dims = None, latent_dim_percentage = 'auto',max_epochs = 1000, max_impute_iters = 100, lr = 1e-3, device = 'cpu', batch_size = 32):
         self.n_layers = n_layers
         self.hidden_dims = hidden_dims
         self.latent_dim_percentage = latent_dim_percentage
+        self.max_epochs = max_epochs
+        self.max_impute_iters = max_impute_iters
+        self.lr = lr
+        self.device = device
+        self.batch_size = batch_size
         super().__init__(missing_values)
 
-    def fit(self, X, max_epochs = 1000, lr = 1e-3, device = 'cpu', batch_size = 32, verbose = False, **kwargs):
-
-        if device == 'cuda' and not torch.cuda.is_available():
+        if self.device == 'cuda' and not torch.cuda.is_available():
            self.device = 'cpu'
            warnings.warn("device = 'cuda' is specified, but no avaliable cuda devices were found. switching to cpu. (torch.cuda.is_available() is False)")
         
+
+    def fit(self, X, verbose = False):
+
         X = self._format_input(X)
         
         incomplete_rows_mask = np.isnan(X).any(axis=1)
         
-        dataset = TensorDataset(torch.tensor(X[~incomplete_rows_mask]))
-        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-        
+      
         self.in_features = X.shape[1]
-                        
+        incomplete_rows_mask = np.isnan(X).any(axis=1)
+
+        dataset = TensorDataset(torch.tensor(X[~incomplete_rows_mask]))
+        dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=True)
+                   
         # if no hidden_dims is specified, reduce layer dimensionality linearly from in_features to a fraction of in_features defined by latent_dim_percentage
         if self.latent_dim_percentage == 'auto':
             latent_dim = int(self.in_features**0.75)
@@ -79,12 +85,12 @@ class AEImputer(_BaseImputer):
         if self.hidden_dims == None:          
             self.hidden_dims = list(linear_generator(self.in_features, latent_dim, n_steps = self.n_layers + 1))[1:]
         
-        self.model = AutoEncoder(self.in_features, self.n_layers, self.hidden_dims).to(device)
-        optimizer = optim.Adam(self.model.parameters(), lr=lr)        
+        self.model = AutoEncoder(self.in_features, self.n_layers, self.hidden_dims).to(self.device)
+        optimizer = optim.Adam(self.model.parameters(), lr=self.lr)        
         criterion = nn.MSELoss() 
         
         # AutoEncoder training loop
-        for epoch in range(max_epochs):
+        for epoch in range(self.max_epochs):
             running_loss = 0.0
             for data in dataloader:
                 batch = data[0]
@@ -100,11 +106,8 @@ class AEImputer(_BaseImputer):
             if verbose:
                 print(f"Epoch {epoch+1}, Loss: {running_loss/len(dataloader)}")
             self.tolerance = running_loss/len(dataloader)
-        return self
         
-    def transform(self, X, batch_size = 32, max_iters = 100, verbose = False, **kwargs):
-        
-        
+    def transform(self, X, verbose = False):
         
         X = self._format_input(X)   
         
@@ -115,7 +118,7 @@ class AEImputer(_BaseImputer):
         self.model.eval()
 
         dataset = TensorDataset(torch.tensor(X[incomplete_rows_mask]))
-        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+        dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=False)
         criterion = nn.MSELoss() 
         
         imputed_batches = []
@@ -129,7 +132,7 @@ class AEImputer(_BaseImputer):
             batch[nan_mask] = torch.randn(batch[nan_mask].shape) 
             
 
-            for epoch in range(max_iters):
+            for epoch in range(self.max_impute_iters):
                 
                 # Input the data to the trained VAE
                 _, imputed_batch = self.model(batch) 
@@ -147,9 +150,7 @@ class AEImputer(_BaseImputer):
                     break
                 
             imputed_batches.append(batch)
-
-        imputed_batches = torch.vstack(imputed_batches).detach().numpy()
-          
-        X[incomplete_rows_mask] = imputed_batches
+        
+        X[incomplete_rows_mask] = torch.vstack(imputed_batches).detach().numpy()
         return X
 
